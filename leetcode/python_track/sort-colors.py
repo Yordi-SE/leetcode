b532a5d0cc67ba2823e6c01ac7497501a4a67e54
Yordanos Lemmawork <111190441+Yordi-SE@users.noreply.github.com>
2024-01-06 22:51:54 +0300

    Added solution for Sort Colors

--- leetcode/python_track/sort-colors.py
+++ leetcode/python_track/sort-colors.py
@@ -3,12 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        swaped = False
-        for i in nums:
-            for j in range(len(nums) - 1):
-                if nums[j] > nums[j + 1]:
-                    nums[j],nums[j + 1] = nums[j + 1], nums[j]
-                    swaped = True
-            if swaped == False:
-                break
-            swaped = False
+        blue,red,white = 0,0,0
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                red += 1
+            elif nums[i] == 1:
+                white += 1
+            else:
+                blue += 1
+        for i in range(len(nums)):
+            for j in range(red):
+                nums[j] = 0
+            for j in range(white):
+                nums[j + red] = 1
+            for j in range(blue):
+                nums[j + red + white] = 2
+            
